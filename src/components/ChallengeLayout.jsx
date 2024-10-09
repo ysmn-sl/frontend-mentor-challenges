8d7dffee42ecac2546d5168c1153fea074b5cb48
yasaman <ysmn.dev@gmail.com>
2024-10-09 22:39:46 +0330

    Added ChallengeLayout component for handling individual challenge layout

---
+++ src/components/ChallengeLayout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ChallengeLayout = ({ title, description, children }) => {
+  return (
+    <div className="container mx-auto ">
+      <h1 className="text-3xl font-bold mb-4">{title}</h1>
+
+      <div className="border rounded-lg shadow-md p-4">{children}</div>
+    </div>
+  );
+};
+
+export default ChallengeLayout;
